2d3805244a3287b05d7f83b4a0415918aa3f3591
saiganesh211001 <saiganesh.ponugupati@zessta.com>
2023-02-06 18:16:05 +0530

    first django project

---
+++ first_project/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    my_dict={'insert_me':"Hello i am from view.py"}
+    return render(request,"index.html",context=my_dict)
+
+# Create your views here.
